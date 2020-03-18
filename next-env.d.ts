ae5581b87e294679508eeacd785e3e5d12b7d27c
Lookaas <lucas@devartisan.cl>
2020-03-18 20:01:38 -0300

    actualización de paquetes en package.json

--- next-env.d.ts
+++ next-env.d.ts
@@ -4,9 +4,3 @@ declare module '*.svg' {
   const content: string;
   export default content;
 }
-
-declare module 'twitter-lite/twitter';
-
-declare module 'react-twitter-auth/lib/react-twitter-auth-component';
-
-declare module 'react-facebook-login';
